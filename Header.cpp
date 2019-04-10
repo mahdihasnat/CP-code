bf691fe91a1ace9f8f95052e578a11f9709a83ac
mahdihasnat <44502454+mahdihasnat@users.noreply.github.com>
2019-04-11 03:25:18 +0600

    update

---
+++ Header.cpp
@@ -0,0 +1,91 @@
+/****** BISMILLAH HIR RAHMANIR RAHIM ******/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+typedef long long ll;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef vector<pair<int,int> > vpii;
+
+// Order Statistic Tree
+
+/* Special functions:
+
+        find_by_order(k) --> returns iterator to the kth largest element counting from 0
+        order_of_key(val) --> returns the number of items in a set that are strictly smaller than our item
+*/
+
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+
+#define EPS 1e-9
+#define NL cout<<endl;
+#define MP make_pair
+#define SORT(a) sort (a.begin(), a.end())
+#define REVERSE(a) reverse (a.begin(), a.end())
+#define ALL(a) a.begin(), a.end()
+#define PI acos(-1)
+#define ms(x,y) memset (x, y, sizeof (x))
+#define pb push_back
+#define DBG(a) cout<< #a <<" --> "<<(a)<<endl;
+#define FastIO ios::sync_with_stdio(false); cin.tie(0);cout.tie(0)
+#define IN freopen("input.txt","r+",stdin)
+#define OUT freopen("output.txt","w+",stdout)
+/* Direction Array */
+
+// int fx[]={1,-1,0,0};
+// int fy[]={0,0,1,-1};
+// int fx[]={0,0,1,-1,-1,1,-1,1};
+// int fy[]={-1,1,0,0,1,1,-1,-1};
+
+template <class T> inline T bigmod(T p,T e,T M)
+{
+    ll ret = 1;
+    for(; e > 0; e >>= 1)
+    {
+        if(e & 1)
+            ret = (ret * p) % M;
+        p = (p * p) % M;
+    }
+    return (T)ret;
+}
+
+template <class T> inline T gcd(T a,T b)
+{
+    if(b==0)
+        return a;
+    return gcd(b,a%b);
+}
+template <class T> inline T modinverse(T a,T M)
+{
+    return bigmod(a,M-2,M);
+}
+
+/****** END OF HEADER ******/
+
+
+int main()
+{
+    FastIO;
+    IN;
+    OUT;
+    int T,cs=0;
+    cin>>T;
+    while(T--)
+    {
+        cout<<"Case "<<++cs<<": ";
+    }
+}
