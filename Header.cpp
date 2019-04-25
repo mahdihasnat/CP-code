7683e0640505135a26513c3848118eb911121d71
mahdihasnat <44502454+mahdihasnat@users.noreply.github.com>
2019-04-25 17:43:02 +0600

    upd

--- Header.cpp
+++ Header.cpp
@@ -11,11 +11,6 @@ using namespace __gnu_cxx;
 
 typedef long long ll;
 typedef pair <int, int> pii;
-typedef pair <ll, ll> pll;
-typedef vector<pair<int,int> > vpii;
-
-// Order Statistic Tree
-
 /* Special functions:
 
         find_by_order(k) --> returns iterator to the kth largest element counting from 0
@@ -34,50 +29,12 @@ ordered_set;
 #define EPS 1e-9
 #define NL cout<<endl;
 #define MP make_pair
-#define SORT(a) sort (a.begin(), a.end())
-#define REVERSE(a) reverse (a.begin(), a.end())
 #define ALL(a) a.begin(), a.end()
-#define PI acos(-1)
-#define ms(x,y) memset (x, y, sizeof (x))
-#define pb push_back
 #define DBG(a) cout<< #a <<" --> "<<(a)<<endl;
 #define FastIO ios::sync_with_stdio(false); cin.tie(0);cout.tie(0)
 #define IN freopen("input.txt","r+",stdin)
 #define OUT freopen("output.txt","w+",stdout)
-/* Direction Array */
 
-// int fx[]={1,-1,0,0};
-// int fy[]={0,0,1,-1};
-// int fx[]={0,0,1,-1,-1,1,-1,1};
-// int fy[]={-1,1,0,0,1,1,-1,-1};
-
-template <class T> inline T bigmod(T p,T e,T M)
-{
-    ll ret = 1;
-    for(; e > 0; e >>= 1)
-    {
-        if(e & 1)
-            ret = (ret * p) % M;
-        p = (p * p) % M;
-    }
-    return (T)ret;
-}
-template <class T> inline T bigpow(T p,T e)
-{
-    ll ret = 1;
-    for(; e > 0; e >>= 1)
-    {
-        if(e & 1)
-            ret = (ret * p);
-        p = (p * p);
-    }
-    return (T)ret;
-}
-
-template <class T> inline T modinverse(T a,T M)
-{
-    return bigmod(a,M-2,M);
-}
 template < class T1,class T2>
 ostream &operator <<(ostream &os,const pair < T1,T2 > &p)
 {
