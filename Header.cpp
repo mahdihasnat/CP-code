2a04c32330fe086ef0d2b89073535d357de0c2db
mahdihasnat <44502454+mahdihasnat@users.noreply.github.com>
2019-06-24 03:41:21 +0600

    added segmented sieve

--- Header.cpp
+++ Header.cpp
@@ -1,34 +1,12 @@
 /****** BISMILLAH HIR RAHMANIR RAHIM ******/
 
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/detail/standard_policies.hpp>
-
 using namespace std;
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
 
 typedef long long ll;
 typedef pair <int, int> pii;
-/* Special functions:
 
-        find_by_order(k) --> returns iterator to the kth largest element counting from 0
-        order_of_key(val) --> returns the number of items in a set that are strictly smaller than our item
-*/
-
-typedef tree<
-int,
-null_type,
-less<int>,
-rb_tree_tag,
-tree_order_statistics_node_update>
-ordered_set;
-
-
-#define EPS 1e-9
 #define NL cout<<endl;
-#define MP make_pair
 #define ALL(a) a.begin(), a.end()
 #define DBG(a) cout<< #a <<" --> "<<(a)<<endl;
 #define FastIO ios::sync_with_stdio(false); cin.tie(0);cout.tie(0)
@@ -41,12 +19,8 @@ ostream &operator <<(ostream &os,const pair < T1,T2 > &p)
     os<<"{"<<p.first<<","<<p.second<<"}";
     return os;
 }
-template < class T1,class T2>
-pair <T1,T2> operator + ( const  pair < T1,T2 > &p1,const pair < T1,T2 > &p2)
-{
-    return make_pair(p1.first+p2.first,p1.second+p2.second);
-}
 /****** END OF HEADER ******/
+
 const int N=1e5+5;
 const int oo=1e9+7;
 
