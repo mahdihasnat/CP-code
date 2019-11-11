8dce01c9a776e3649b7ad864b939e11ed3fb8197
mahdihasnat <44502454+mahdihasnat@users.noreply.github.com>
2019-11-11 19:42:49 +0600

    So remember me; I will remember you | Surah Al-Baqarah 2:152

--- Data Structure/dsu by rank.cpp
+++ Data Structure/dsu by rank.cpp
@@ -1,21 +1,23 @@
 
 int parent[N];
 int rnk[N];
-int find_set(int v) {
-    if (v == parent[v])
-        return v;
-    return parent[v] = find_set(parent[v]);
+int find_set(int v)
+{
+    return parent[v] == v ? v :  parent[v] = find_set(parent[v]);
 }
 
-void make_set(int v) {
+void make_set(int v)
+{
     parent[v] = v;
     rnk[v] = 0;
 }
 
-void union_sets(int a, int b) {
+void union_sets(int a, int b)
+{
     a = find_set(a);
     b = find_set(b);
-    if (a != b) {
+    if (a != b)
+    {
         if (rnk[a] < rnk[b])
             swap(a, b);
         parent[b] = a;
