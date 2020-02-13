b4185ce428e32f55e1c57bf499dfc276be18f294
mahdihasnat <44502454+mahdihasnat@users.noreply.github.com>
2020-02-14 00:10:53 +0600

    Update sparse table.cpp

--- Data Structure/sparse table.cpp
+++ Data Structure/sparse table.cpp
@@ -1,5 +1,6 @@
 const int N=3e5+5;
 const int K = 23 ; /// k >= ceil(lg22(n)) +1
+int arr[N];
 int st[N][K + 1];
 int lg2[N+1];
 void ini()
@@ -8,25 +9,25 @@ void ini()
     for (int i = 2; i <= N; i++)
         lg2[i] = lg2[i/2] + 1;
 }
-int f(const vector<int > &array ,int i,int j)
+int f(int i,int j)
 {
-    if(array[i]==array[j]) return i<j ? j: i;
-    return array[i]<array[j] ? i: j;
+    if(arr[i]==arr[j]) return i<j ? j: i;
+    return arr[i]<arr[j] ? i: j;
 }
 
-void pre(const vector<int> &array, int n)
+void pre( int n)
 {
 
     for (int i = 0; i < n; i++)
-        st[i][0] = f(array, i,i);
+        st[i][0] = f( i,i);
 
     for (int j = 1; j <= K; j++)
         for (int i = 0; i + (1 << j) <= n; i++)
-            st[i][j] = f(array, st[i][j-1], st[i + (1 << (j - 1))][j - 1]);
+            st[i][j] = f( st[i][j-1], st[i + (1 << (j - 1))][j - 1]);
 }
-int getf(const vector<int > &array, int L,int R)
+int getf( int L,int R)
 {
     if(L>R) swap(L,R);
     int j = lg2[R - L + 1];
-    return  f(array,st[L][j], st[R - (1 << j) + 1][j]);
-} 
+    return  f(st[L][j], st[R - (1 << j) + 1][j]);
+}
