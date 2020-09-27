2d9da61c28ab103b4497f8ee51d419be8e483ffa
mahdihasnat <44502454+mahdihasnat@users.noreply.github.com>
2020-09-27 08:03:19 +0600

    Update Segment Tree Lazy Array.cpp

--- Data Structure/Segment Tree Lazy Array.cpp
+++ Data Structure/Segment Tree Lazy Array.cpp
@@ -1,78 +1,65 @@
 const int N= 1e5+5;
 int value[4*N];
 int lazy[4*N];
-void UpdateRange(int node , int ss, int se, int us,int ue, int diff)
+
+void propagate(int u,int ss,int se)
+{
+    value[u]+= (ss-se+1) * lazy[u];
+    if(ss!=se)
+        lazy[u<<1] += lazy[u] , lazy[u<<1|1]+= lazy[u];
+    lazy[u] = 0;
+}
+int func(int x,int y)
+{
+    return x+y;
+}
+
+void UpdateRange(int u , int ss, int se, int us,int ue, int diff)
 {
     int mid = ss+(se-ss)/2;
 
-    if (lazy[node] != 0)
-    {
-        value[node] += (se-ss+1)*lazy[node];
-        if (ss != se)
-        {
-            lazy[2*node]+=lazy[node];
-            lazy[2*node+1]+=lazy[node];
-        }
-        lazy[node] = 0;
-    }
+    propagate(u,ss,se);
 
     if (ss>se || ss>ue || se<us)
         return ;
 
     if (ss>=us && se<=ue)
     {
-        value[node] += (se-ss+1)*diff;
-
-        if (ss != se)
-        {
-            lazy[2*node]+=diff;
-            lazy[2*node+1]+=diff;
-        }
-        return;
+        lazy[u] += diff;
+        return propagate(u,ss,se);
     }
 
 
-    UpdateRange(2*node, ss, mid, us, ue, diff);
-    UpdateRange(2*node+1, mid+1, se, us, ue, diff);
+    UpdateRange(u<<1, ss, mid, us, ue, diff);
+    UpdateRange(u<<1|1, mid+1, se, us, ue, diff);
 
-    value[node]=value[2*node]+value[2*node+1];
+    value[u]=func(value[u<<1],value[u<<1|1]);
 
 }
-int GetSum(int node,int ss, int se, int qs, int qe)
+int GetFunc(int u,int ss, int se, int qs, int qe)
 {
 
     int mid = ss+(se-ss)/2;
-
-    if (lazy[node] != 0)
-    {
-        value[node] += (se-ss+1)*lazy[node];
-        if (ss != se)
-        {
-            lazy[2*node]+=lazy[node];
-            lazy[2*node+1]+=lazy[node];
-        }
-        lazy[node] = 0;
-    }
 
     if (ss>se || ss>qe || se<qs)
         return 0;
 
     if (ss>=qs && se<=qe)
-        return value[node];
+        return value[u];
 
-    return GetSum(2*node,ss,mid,qs,qe)+GetSum(2*node+1,mid+1,se,qs,qe);
+    return func(GetFunc(u<<1,ss,mid,qs,qe),GetFunc(u<<1|1,mid+1,se,qs,qe));
 }
 
-void Build(int node,int *a,int ss,int se)
+void Build(int u,int *a,int ss,int se)
 {
-    lazy[node]=0;
+    lazy[u]=0;
     if(ss==se)
     {
-        value[node]=a[ss];
+        value[u]=a[ss];
         return ;
     }
     int mid = ss+(se-ss)/2;
-    Build(2*node,a,ss,mid);
-    Build(2*node+1,a,mid+1,se);
-    value[node]=value[2*node]+value[2*node+1];
+    Build(u<<1,a,ss,mid);
+    Build(u<<1|1,a,mid+1,se);
+    value[u]=func(value[u<<1],value[u<<1|1]);
 }
